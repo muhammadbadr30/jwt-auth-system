7b7f1860dbd5eb8ca391971bd98d0fe15ac62317
MuhammadBadr <muhammadbadr30@gmail.com>
2020-08-29 13:56:53 +0200

    jsw token is created for users

--- controllers/authControllers.js
+++ controllers/authControllers.js
@@ -1,4 +1,6 @@
 const User = require('../models/User');
+const jwt = require('jsonwebtoken');
+
 
 // handling email and password errors 
 const handleErrors = (err) => {
@@ -32,6 +34,12 @@ const handleErrors = (err) => {
     return errors;
 }
 
+// Creating token
+const maxAge = 3 * 24 * 60 * 60 
+const createToken = (id) => {
+    return jwt.sign({id}, process.env.SECRET, {expiresIn: maxAge})
+}
+
 
 module.exports.signup_get = (req, res) => {
     res.render('signup');
@@ -49,6 +57,12 @@ module.exports.signup_post = async (req, res) => {
         // creating user using User model
         const user = await User.create({email, password})
 
+        // creating a token for the user
+        const token = createToken(user._id)
+
+        // sending cookies with token to the browser
+        res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge * 1000})
+
         // sending response with success status code
         res.status(201).json({user: user._id})
     } catch (err) {
@@ -62,6 +76,10 @@ module.exports.login_post = async (req, res) => {
 
     try {
         const user = await User.login(email, password);
+
+        const token = createToken(user._id);
+
+        res.cookie('jwt', token, {httpOnly: true, maxAge: maxAge * 1000})
 
         res.status(200).json({user: user._id})
     } catch (err) {
