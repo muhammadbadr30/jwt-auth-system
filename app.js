f7bda053bb3a2771626873efa3e102927714aa11
MuhammadBadr <muhammadbadr30@gmail.com>
2020-08-29 10:18:12 +0200

    initialized the server and created the basic views

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const mongoose = require('mongoose');
+require('dotenv').config();
+
+
+const app = express();
+
+// Middlewares
+
+app.use(express.static('public'))
+app.use(express.json())
+
+
+// view engine
+app.set('view engine', 'ejs')
+
+// database connection 
+mongoose.connect(process.env.dbURI, {useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true})
+    .then(result => app.listen(process.env.PORT))
+    .catch(err => console.log(err))
+
+
+// routes 
+app.get('/', (req, res) => res.render('home'))
+app.get('/smoothies', (req, res) => res.render('smoothies'))
+
